6c12727689fe031afa3f5835a473c132c372209e
Judy McGoldrick <jjeanius@verizon.net>
2017-12-14 01:50:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,14 +6,18 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |element|
-"#{element}!".capitalize
+    "#{element}!".capitalize
+  end
 end
 
+def long_planeteer_calls(calls)
+  if calls>array.new(4)
+    true
+  else
+    false
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
