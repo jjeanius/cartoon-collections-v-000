4117ca505e3e923d1e1379b51d65d2c3bccea8ea
Judy McGoldrick <jjeanius@verizon.net>
2017-12-14 03:52:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ end
 
 def find_the_cheese(cheese_types)
     cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.detect{|i| i.cheddar?}
+  if cheese_types.detect{|i| i.cheese_types?}
     return "#{cheese_types}"
   else
     nil
