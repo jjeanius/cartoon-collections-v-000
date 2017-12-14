61d6a56ba7b95401dbf34f458d8bb085dd0c3c2d
Judy McGoldrick <jjeanius@verizon.net>
2017-12-14 00:48:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet.collect do|planeteer_calls| 
+def summon_captain_planet.collect do|planeteer_calls|
   "#{planeteer_calls}!".upcase
 end
 
