5ebba25e5eec6419e932bac578620461249edb24
Judy McGoldrick <jjeanius@verizon.net>
2017-12-14 01:12:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |element|
-  "#{element}".upcase.split("!")
-end
+  "#{element}"
+end.upcase("!")
 end
 
 def long_planeteer_calls# code an argument here
