fd559427a6558f33b7087db28b2d0b437d738424
Judy McGoldrick <jjeanius@verizon.net>
2017-12-14 03:30:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.include? cheese_types
+  if cheese_types == "chaddar"
     return "#{cheese_types}"
   else
     nil
